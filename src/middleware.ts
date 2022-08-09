cd1a14cf37f791d2da73e882f04b295d08958413
Jingxian <jingxianchai01@gmail.com>
2022-08-09 14:37:50 -0500

    chore: redirect middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export default async function middleware(req: NextRequest) {
+  const slug = req.nextUrl.pathname.split('/').pop()
+
+  const dataFetch = await fetch(`${req.nextUrl.origin}/api/redirect/${slug}`)
+
+  if (dataFetch.status === 404) {
+    return NextResponse.redirect(req.nextUrl.origin)
+  }
+
+  const link = await dataFetch.json()
+
+  if (link?.url) {
+    return NextResponse.redirect(link.url)
+  }
+}
+
+// config with custom matcher
+export const config = {
+  matcher: '/api/redirect/:slug',
+}
