427c8131915eb2aa27ba09707c57a4e11ded286e
Jingxian <jingxianchai01@gmail.com>
2022-08-10 01:38:50 -0500

    chore: link trpc router

---
+++ src/server/router/link.ts
@@ -0,0 +1,49 @@
+import { createRouter } from './context'
+import { z } from 'zod'
+
+export const linkRouter = createRouter()
+  .query('verifySlug', {
+    input: z.object({
+      slug: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const link = await ctx.prisma.link.findUnique({
+        where: {
+          slug: input.slug,
+        },
+      })
+      if (link) {
+        return {
+          success: false,
+          msg: 'slug already used',
+        }
+      }
+      return {
+        success: true,
+      }
+    },
+  })
+  .mutation('createLink', {
+    input: z.object({
+      slug: z.string(),
+      url: z.string(),
+    }),
+    async resolve({ input: { slug, url }, ctx }) {
+      try {
+        await ctx.prisma.link.create({
+          data: {
+            slug,
+            url,
+          },
+        })
+
+        return {
+          success: true,
+        }
+      } catch (err) {
+        return {
+          success: false,
+        }
+      }
+    },
+  })
